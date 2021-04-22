d7ab2649aa952e6a2b9f15ceeb7600466dd2b916
virajrg108 <vrgawde_b16@et.vjti.ac.in>
2021-04-22 15:15:58 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import ReactPlayer from "react-player";
 
 import "./style.css";
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-      <ReactPlayer
-        url="https://static.videezy.com/system/resources/previews/000/019/695/original/pointing-pink.mp4"
-        playing="true"
-        loop="true"
-      />
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Subtitle Editor</h1>
+        <ReactPlayer
+          url="https://static.videezy.com/system/resources/previews/000/019/695/original/pointing-pink.mp4"
+          playing="true"
+          loop="false"
+          width="720px"
+          playIcon={<div>1</div>}
+        />
+      </div>
+    );
+  }
 }
